ef99efbc120d103bc96d0a9377d02b5b87e30ced
ymywsw-alt <ymywsw@gmail.com>
2026-01-30 15:42:05 +0900

    fix-render-ffmpeg-static

--- render-build.sh
+++ render-build.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-# Install deps
+# install node deps
 npm install
 
-# Install ffmpeg (Render Debian/Ubuntu base)
-apt-get update
-apt-get install -y ffmpeg
+# install static ffmpeg binary
+mkdir -p /opt/ffmpeg
+cd /opt/ffmpeg
+
+curl -L https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -o ffmpeg.tar.xz
+tar -xf ffmpeg.tar.xz
+cp ffmpeg-*/ffmpeg /usr/local/bin/ffmpeg
+
+ffmpeg -version
